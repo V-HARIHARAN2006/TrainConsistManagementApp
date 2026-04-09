e22acc446f12dabf291bdaad810120289411a2cc
Hari Haran V <hari230706@gmail.com>
2026-04-09 09:28:11 +0530

    HariHaran Added UC1 Train Consist Initialization

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
-        System.out.println("Train Consist Management App Started");
+
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train consist initialized.");
+
+        System.out.println("Initial bogie count: " + trainConsist.size());
     }
 }
